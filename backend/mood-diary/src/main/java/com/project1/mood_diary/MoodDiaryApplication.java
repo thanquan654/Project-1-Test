124f98d44c4979120fa359d046a58ff1bc717a25
LeDanhTien <danhtien2k4@gmail.com>
2025-09-26 15:47:34 +0700

    chore: cài đặt thư viện cơ bản, feat: thêm chức năng login

---
+++ backend/mood-diary/src/main/java/com/project1/mood_diary/MoodDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.project1.mood_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodDiaryApplication.class, args);
+	}
+
+}
